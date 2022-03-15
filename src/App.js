ceefea73dc42dbadc5564be7635d5ad360362d5e
Natasha Enriquez <95707854+TashaEnriquez@users.noreply.github.com>
2022-03-15 09:23:31 +0100

    add components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Routes, Route, Link } from "react-router-dom";
 import Layout from "./components/Layout";
+import Home from "./components/Home";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<div>All Posts</div>} />
+        <Route index element={<Home />} />
         <Route path="new-post" element={<div>Tell us about your pet!</div>} />
       </Route>
     </Routes>
