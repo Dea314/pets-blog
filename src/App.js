c12ba515e1d40ce3a311fad13e6647ce38b73f44
Dea314 <fabrisdanijela@gmail.com>
2022-03-16 15:52:58 +0100

    connection with contentful

--- src/App.js
+++ src/App.js
@@ -4,15 +4,37 @@ import Home from "./components/Home";
 import Contact from "./components/Contact";
 import About from "./components/About";
 import NotFound from "./components/NotFound";
-
+import { createClient } from "contentful";
+import { useState, useEffect } from "react";
 
 const App = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+
+    const client = createClient({
+      space: process.env.REACT_APP_SPACE_ID,
+      accessToken: process.env.REACT_APP_ACCESS_TOKEN,
+    });
+    client
+      .getEntries()
+      .then((entries) => {
+        console.log(entries);
+        setPosts(entries.items);
+        console.log(entries.items);
+      })
+      .catch((err) => console.log(err));
+    setLoading(false);
+  }, []);
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Home/>} />
-        <Route path="new-comment" element={<Contact/>} />
-        <Route path="about" element={<About/>} />
+        <Route index element={<Home />} />
+        <Route path="new-comment" element={<Contact />} />
+        <Route path="about" element={<About />} />
         <Route path="*" element={NotFound} />
       </Route>
     </Routes>
