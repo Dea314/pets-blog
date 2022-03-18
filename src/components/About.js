38da330513434fd53fcdc0d8aa32d22938ca6d01
Steffani <sartinisteffani@gmail.com>
2022-03-18 16:59:15 +0100

    Content added to About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,17 @@
 const About = () => {
-    return <div>About Us</div>;
-};
-
-export default About;
+    return <div>
+    
+        <h1>About Us</h1>
+        <br/>
+        <h3>We are happy to have you here, and we can surely assume that you are also crazy about cute and fluffy creatures like us. 
+        We are a team of three pet lovers who want to bring awareness about how animals save lives and how important they are in times 
+        like this. So here is a hint about our background. 
+        We are brand new Web Developers with a huge mission this year: to put our learning into action and transform ideas into dreams. 
+        So, why not use our talent for something we'd love to share with the world? Meaning, we're bringing a community of pet lovers to 
+        share funny and exciting tips about our kitties and doggos and we want you to be part of it. Feel free to contact us and share your comments. 😀 </h3>
+        
+    </div>;
+  };
+  
+  
+  export default About;
