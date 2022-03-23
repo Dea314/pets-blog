babfd24da15d6143a43d6a92b8a4d220898e9874
Natasha Enriquez <95707854+TashaEnriquez@users.noreply.github.com>
2022-03-23 16:34:48 +0100

    404

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,7 +1,25 @@
+import { Navigate, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
 const NotFound = () => {
-  return <div>Oops, page not found!</div>;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => navigate("/Home"), 9000);
+  }, [navigate]);
+
+  return (
+    <div>
+      <h2>Opps, page not found!</h2>
+      <h4>We're redirecting you to Home Page now!</h4>
+      <img
+        src="https://i.gifer.com/7iJR.gif"
+        alt="404"
+        width="1000"
+        height="600"
+      />
+    </div>
+  );
 };
 
 export default NotFound;
-
-// Set timer that redirects to homepage after 30 sec?
