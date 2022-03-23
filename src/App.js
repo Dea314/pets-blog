bd77a71ae0330b38678efb9bf8c23ac425616298
Natasha Enriquez <95707854+TashaEnriquez@users.noreply.github.com>
2022-03-23 16:29:12 +0100

    add text

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import About from "./components/About";
 import NotFound from "./components/NotFound";
 import Fun from "./components/Fun";
 import NewComment from "./components/NewComment";
+import SinglePost from "./components/SinglePost";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="post/:id" element={<SinglePost />} />
         <Route path="fun" element={<Fun />} />
         <Route path="about" element={<About />} />
         <Route path="contact" element={<Contact />} />
--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import NewComment from "./NewComment";
+import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
 
 const SinglePost = () => {
   return <NewComment />;
